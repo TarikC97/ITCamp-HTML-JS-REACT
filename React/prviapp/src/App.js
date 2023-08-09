1d9dfaaa03f7a8c102741407eb9c60f86d9c26fd
unknown <tarikcuric97@gmail.com>
2023-08-09 11:32:34 +0200

    IT-CAMP 09-08-2023

--- React/prviapp/src/App.js
+++ React/prviapp/src/App.js
@@ -1,17 +1,33 @@
 import { useState } from 'react';
 import './App.css';
-// import Card from './components/Card';
-import Button from './components/Button'
+import Card from './components/Card';
+import { customProducts } from './data/data';
+// import Button from './components/Button';
 
 function App() {
 
-  const [broj,setBroj] = useState([1,2,3,4,5,6,7,8,9,10])
+  const [data,setData] = useState([])
+
+  //Domaci return filtered phones from products, u Card
+  //U vrh ugla visinaPopusta
+  //title,brand,price u body Carda.
+  // const [broj,setBroj] = useState([1,2,3,4,5,6,7,8,9,10])
+  // console.log(customProducts)
+  const fetchData =()=>{ 
+    fetch("http://dummyjson.com/products")
+       .then((res)=>res.json())
+       .then((data) => setData(data.products))
+}
+console.log(data)
 
   return (
     <div className="app">
-      {broj.map((el)=>{
+      {data.map((product)=> product.price>500)}
+      {/* {customProducts.map((product,i)=> <Card key={i} name={product.title} desc={product.description} price={product.price} />)} */}
+      {/* {broj.map((el)=>{
         return <Button  body={el}/>
-      })}
+      })} */}
+      <button onClick={()=> fetchData()}>Fetch data</button>
       {/* <button className='button'onClick={()=>{setBroj(()=>broj+1)}}>Increment</button>
       {broj}
       <button className='button'onClick={()=>{setBroj(()=>broj-1)}}>Decrement</button> */}
---
+++ React/prviapp/src/data/data.js
@@ -0,0 +1,17 @@
+export const customProducts =[
+    {
+        title:"Air Force 1",
+        description: "Old fashioned sneakers for every day!",
+        price:"150$",
+    },
+    {
+        title:"Adidas Superstar",
+        description: "New brand for brand new modern looks!",
+        price:"120$",
+    },
+    {
+        title:"Puma Sport1",
+        description: "Most comfortable shoes in store!",
+        price:"100$",
+    },
+]
