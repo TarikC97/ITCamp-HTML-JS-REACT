77c6e28d6c20c43dbc40c6a3f6d4b0f607b1c396
unknown <tarikcuric97@gmail.com>
2023-09-13 18:12:53 +0200

    IT-CAMP 13-09-2023 REACT

--- React/prviapp/src/App.js
+++ React/prviapp/src/App.js
@@ -1,64 +1,12 @@
-import { useState } from 'react';
 import './App.css';
 import Card from './components/Card';
-// import { customProducts } from './data/data';
-// import Button from './components/Button';
 
 function App() {
-
-  const [data,setData] = useState([])
-
-  //Domaci return filtered phones from products, u Card
-  //U vrh ugla visinaPopusta
-  //title,brand,price u body Carda.
-  // const [broj,setBroj] = useState([1,2,3,4,5,6,7,8,9,10])
-  // console.log(customProducts)
-  const fetchData =()=>{ 
-    fetch("http://dummyjson.com/products")
-       .then((res)=>res.json())
-       .then((data) => setData(data.products))
-}
-console.log(data)
-
   return (
-    <>
+    
     <div className="app">
-      {data.map((product,i)=> product.category ==='smartphones' ? 
-      <Card key={i} 
-      name={product.title} 
-      desc={product.description} 
-      price={product.price}
-      discount={product.discountPercentage}
-      brand={product.brand}
-      stock={product.stock}
-      />: console.log('Not smartphone!'))}
-      
-      {/* {customProducts.map((product,i)=> <Card key={i} name={product.title} desc={product.description} price={product.price} />)} */}
-      {/* {broj.map((el)=>{
-        return <Button  body={el}/>
-      })} */}
-      {/* <button className='button'onClick={()=>{setBroj(()=>broj+1)}}>Increment</button>
-      {broj}
-      <button className='button'onClick={()=>{setBroj(()=>broj-1)}}>Decrement</button> */}
-      {/* <Card 
-      image1={<img className='img1' src={require('./slika2.jpeg')} alt="slika1" />} 
-      name="Air Force 2" 
-      desc="Branding shoe from Nike always on Top since 1995 to present."
-      price="240$"  />
-      <Card 
-      image1={<img className='img1' src={require('./slika5.jpg')} alt="slika1" />} 
-      name="Adidas Superstar" 
-      desc="Branding shoe from Adidas always on Top since 2001 to present."
-      price="150$" />
-      <Card 
-      image1={<img className='img1' src={require('./slika6.jpg')} alt="slika1" />} 
-      name="Puma Sport 1" 
-      desc="Branding shoe from Puma always on Top since 2003 to present."
-      price="120$"
-       /> */}
+      <Card  ime="Tarik" prezime="Curic"/>    
     </div>
-          <button className='button2' onClick={()=> fetchData()}>Fetch data</button>
-    </>
   );
 }
 
--- React/prviapp/src/components/Card.js
+++ React/prviapp/src/components/Card.js
@@ -1,34 +1,13 @@
 import React from 'react'
-import "./card.css"
-import Button from './Button'
 
 const Card = (props) => {
-
-  
-// const {price}= props
-//Destructring object
- //const {ime,age} = props
+    var ime = ""
+    var prezime = ""
   return (
-    <div className="card">
-      <h1>Title:{props.name}</h1>
-      <h3>
-        Description:{props.desc}
-      </h3>
-      <h3>
-        Price:{props.price}$
-      </h3>
-      <h3>
-        Discount:{props.discount}%
-      </h3>
-      <h3>
-        Brand:{props.brand}
-      </h3>
-      <h3>
-        Stock:{props.stock}
-      </h3>
-
-    <Button className='button1' body={'Add to the Cart'} />
-    </div>
+    <>
+    <h1>Ime:{props.ime}</h1>
+    <h1>Prezime:{props.prezime}</h1>
+    </>
   )
 }
 
