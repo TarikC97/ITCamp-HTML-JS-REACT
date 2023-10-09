26a812f3f6b0657a231e2bf113f1e7c5e1461b17
unknown <tarikcuric97@gmail.com>
2023-10-09 18:20:08 +0200

    IT CAMP 09-10-2023

--- React/prviapp/src/components/Async.js
+++ React/prviapp/src/components/Async.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Async = () => {
   const [dataJson, setDataJson] = useState([])
@@ -7,9 +7,19 @@ const Async = () => {
       .then((res) => res.json())
       .then((data) => setDataJson(data.products))
   }
+  useEffect(() => {
+    fetchData()
+  }, [])
   console.log(dataJson)
   return (
     <div>
+      <input type='text' placeholder='Search...' />
+      <button
+        style={{ backgroundColor: 'red', width: '70px' }}
+        onClick={() => console.log('Klik')}
+      >
+        Search
+      </button>
       <button onClick={fetchData}>Fetch Data</button>
       <button
         onClick={() => setDataJson(dataJson.filter((el) => el.id % 2 === 0))}
